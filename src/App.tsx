a4d795489614d658e69ba761dd60d1aa8416e870
파벨 <pavel@pabels-MacBook-Pro.local>
2025-10-24 19:16:11 +0900

    add new super feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import MusicList from "./components/MusicList";
+import NewSuperFeature from "./components/NewSuperFeature";
 
 // const tracks = [
 //   { id: 1, name: "Fun soundtrack" },
@@ -9,7 +10,11 @@ import MusicList from "./components/MusicList";
 function App() {
   return (
     <div className="text-3xl">
-      Super Music App version 2!
+      Super Music App version 3!
+      <div>
+        <p>Some new awesome feature added!</p>
+      </div>
+      <NewSuperFeature />
       <MusicList />
     </div>
   );
